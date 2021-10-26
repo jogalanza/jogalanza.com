e0a168cbe1a099008a0a4cca17d3dadc9bf06ea0
jogalanza <44076387+jogalanza@users.noreply.github.com>
2021-10-26 09:18:49 +0800

    initial commit

---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-page class="">
+    <div class="row justify-center">
+      <div class="col-12 col-md-10">
+        <div class="row">
+          <div class="col-12 col-md-6 q-px-md q-my-auto">
+        <div class="name-header">
+          Hi, I'm Jared Omni Galanza
+        </div>
+        <div class="name-header caption">
+          {{ `<full-stack-developer />` }}
+        </div>
+      </div>
+      <div class="col-12 col-md-6">
+        <div class=".tagcloud" ref="tg">
+
+        </div>
+      </div>
+        </div>
+      </div>
+      
+    </div>
+    
+    
+  </q-page>
+</template>
+
+<script>
+//import TagCloud from "TagCloud"
+const TagCloud = require("TagCloud")
+import { defineComponent, onMounted, ref } from 'vue';
+
+export default defineComponent({
+  name: 'AboutMe',
+  setup(){
+    const tagTexts = ["VueJS", "Node JS", "ASP.NET", "SQL", "Android", "Cordova", "Capacitor", "HTML", "CSS", "Javascript", "Python", "ES5/ES6", "npm", "REST", "Websockets", "jQuery", "Git", "Java", "C++", "Delphi"]
+    const tg = ref(null)
+    const tgOptions = {
+      radius: 350,
+      maxSpeed: "slow",
+      initSpeed: "slow"
+    }
+    onMounted(() => {
+
+      setTimeout(() => {
+        TagCloud(tg.value, tagTexts, tgOptions);
+      }, 300)
+      
+    })
+
+    return {
+      tg
+    } 
+  }
+})
+</script>
