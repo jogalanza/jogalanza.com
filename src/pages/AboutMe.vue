fa6fff92540bbb769131175842c4b23d9efb2215
jogalanza <44076387+jogalanza@users.noreply.github.com>
2021-10-29 22:06:22 +0800

    add tagcloud

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -3,7 +3,7 @@
     <div class="row justify-center">
       <div class="col-12 col-md-10">
         <div class="row">
-          <div class="col-12 col-md-6 q-px-md q-my-auto">
+          <div class="intro col-12 col-md-6 q-px-md q-my-auto">
         <div class="name-header">
           Hi, I'm Jared Omni Galanza
         </div>
@@ -12,7 +12,7 @@
         </div>
       </div>
       <div class="col-12 col-md-6">
-        <div class=".tagcloud" ref="tg">
+        <div class=".tagcloud row justify-center" ref="tg">
 
         </div>
       </div>
@@ -33,7 +33,7 @@ import { defineComponent, onMounted, ref } from 'vue';
 export default defineComponent({
   name: 'AboutMe',
   setup(){
-    const tagTexts = ["VueJS", "Node JS", "ASP.NET", "SQL", "Android", "Cordova", "Capacitor", "HTML", "CSS", "Javascript", "Python", "ES5/ES6", "npm", "REST", "Websockets", "jQuery", "Git", "Java", "C++", "Delphi"]
+    const tagTexts = ["VueJS", "Node JS", "ASP.NET", "SQL", "Android", "Cordova", "Capacitor", "HTML", "CSS", "Javascript", "Python", "ES5/ES6", "npm", "REST", "websockets", "jQuery", "Git", "Java", "C#", "Delphi", "Polymer JS"]
     const tg = ref(null)
     const tgOptions = {
       radius: 350,
@@ -54,3 +54,11 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped lang="scss">
+@media (max-width: 1023px) {
+  .intro{
+    text-align: center;
+  }
+}
+</style>
